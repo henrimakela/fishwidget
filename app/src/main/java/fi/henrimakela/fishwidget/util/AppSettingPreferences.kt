ff489f652e4d5bd0a11b086de15fdef5305f70fd
Henri Mäkelä <henri.makela@boogiesoftware.com>
2020-11-28 16:13:53 +0200

    App has theme switch setting

--- app/src/main/java/fi/henrimakela/fishwidget/util/AppSettingPreferences.kt
+++ app/src/main/java/fi/henrimakela/fishwidget/util/AppSettingPreferences.kt
@@ -16,37 +16,71 @@ import fi.henrimakela.fishwidget.R
 class AppSettingPreferences(context: Context) :
     SharedPreferences.OnSharedPreferenceChangeListener {
 
-    companion object{
+    companion object {
         private val PREFERENCE_UNIT_KEY = "unit"
+        private val PREFERENCE_THEME_KEY = "theme"
         val PREFERENCE_UNIT_METRIC = "metric"
         val PREFERENCE_UNIT_IMPERIAL = "imperial"
     }
+
     private val sharedPreferences =
         context.getSharedPreferences(
             context.getString(R.string.fishwidget_settings_preferences),
             Context.MODE_PRIVATE
         )
     private var _unit = MutableLiveData<String>()
+    private var _useDarkMode = MutableLiveData<Boolean>()
+
     val unit: LiveData<String> get() = _unit
+    val useDarkMode: LiveData<Boolean> get() = _useDarkMode
+
 
     init {
         _unit.postValue(getWeatherDataUnitPreference())
+        _useDarkMode.postValue(isUsingDarkMode())
         sharedPreferences.registerOnSharedPreferenceChangeListener(this)
     }
 
+    fun unregisterSharedPreferencesListener(){
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
     fun saveWeatherDataUnitPreference(unit: String) {
+        _unit.postValue(unit)
         sharedPreferences.edit().putString(PREFERENCE_UNIT_KEY, unit).apply()
+    }
+
+    fun saveThemeModePreference(useDarkMode: Boolean) {
+        sharedPreferences.edit().putBoolean(PREFERENCE_THEME_KEY, useDarkMode).apply()
+        _useDarkMode.postValue(useDarkMode)
     }
 
     private fun getWeatherDataUnitPreference(): String? {
         return sharedPreferences.getString(PREFERENCE_UNIT_KEY, PREFERENCE_UNIT_METRIC)
     }
 
+    fun isUsingDarkMode(): Boolean {
+        return sharedPreferences.getBoolean(PREFERENCE_THEME_KEY, false)
+    }
+
+    fun noSettingsConfigured(): Boolean{
+        return sharedPreferences.all.isEmpty()
+    }
+
     override fun onSharedPreferenceChanged(prefs: SharedPreferences?, key: String?) {
         key?.let {
-            if (it == PREFERENCE_UNIT_KEY) {
-                prefs?.let {
-                    _unit.postValue(it.getString(key, PREFERENCE_UNIT_METRIC))
+            when (it) {
+                PREFERENCE_UNIT_KEY -> {
+                    prefs?.let {
+                        _unit.postValue(it.getString(key, PREFERENCE_UNIT_METRIC))
+                    }
+                }
+                PREFERENCE_THEME_KEY -> {
+                    prefs?.let {
+                        _useDarkMode.postValue(it.getBoolean(key, false))
+                    }
+                }
+                else -> {
                 }
             }
         }
